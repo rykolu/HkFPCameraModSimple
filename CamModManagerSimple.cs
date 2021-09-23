f17dd05f7fb6137d92a3a981dbf107b4605d4568
rykolu <ryko.kohne@hotmail.com>
2021-09-23 16:14:25 -0700

    forgot to upload the files

---
+++ CamModManagerSimple.cs
@@ -0,0 +1,100 @@
+﻿using BepInEx;
+using UnityEngine;
+using Amplitude.Mercury.Presentation;
+
+namespace HkFPCameraModSimple
+{
+    [BepInPlugin("humankind.rykolu.HkFPCameraModSimple", "Humankind FPCamera Mod Simple", "1.0.0.0")]
+    public class CamModManagerSimple : BaseUnityPlugin
+    {
+        public bool cameraBool;
+        private float nearClip = 33.93487f; //orig values
+        private float farClip = 113.942f;
+
+        private GameObject cameraGo;
+        private PresentationCameraMover presCamMover;
+        private FPcameraSimple FPcam;
+        private Camera cameraCam;
+
+        private void CameraSetup()
+        {
+            cameraBool = false;
+            cameraGo = GameObject.Find("Camera");
+            if (cameraGo == null)
+                { Logger.LogError("NullRef, No Camera GameObject found"); }
+
+            //this is the default script the game uses to control the camera
+            presCamMover = cameraGo.GetComponent<PresentationCameraMover>();
+            if (presCamMover == null)
+                { Logger.LogError("NullRef, No PresentationCameraMover found"); }
+
+            FPcam = cameraGo.AddComponent<FPcameraSimple>();
+            if (FPcam == null) 
+                { Logger.LogError("NullRef, FPcamera script did not load"); }
+
+            cameraCam = cameraGo.GetComponent<Camera>();
+            if (cameraCam == null)
+                { Logger.LogError("NullRef, No camera \"Camera\" component found"); }
+
+            nearClip = cameraCam.nearClipPlane;   //in case of different camera settings
+            farClip = cameraCam.farClipPlane;
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F2))
+            {
+                if (!cameraGo)
+                {
+                    CameraSetup();
+                }
+
+                cameraBool = !cameraBool;
+                presCamMover.enabled = !cameraBool;
+                FPcam.enabled = cameraBool;
+                //GUI.enabled = !GUI.enabled;
+
+                if (cameraBool == true) 
+                { 
+                    UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+                    cameraCam.nearClipPlane = 0.01f;
+                    cameraCam.farClipPlane = farClip * 2;
+                }
+                else 
+                { 
+                    UnityEngine.Cursor.lockState = CursorLockMode.None;
+                    cameraCam.nearClipPlane = nearClip;
+                    cameraCam.farClipPlane = farClip;
+                }
+            }
+
+            //ALL CAMERAS IN SCENE:
+            //ImpostorCamera
+            //AvatarCamera
+            //Camera        <---
+            //MainView
+            //UIFxCamera
+
+
+
+            //ALL RENDER LAYERS IN SCENE:
+            //Default
+            //TransparentFX
+            //Ignore Raycast
+
+            //Water
+            //UI
+
+            //Overlay
+            //PostProcess
+            //PresentationCursorTarget
+            //AvatarVisible
+            //AvatarHidden
+            //PresentationPawn
+            //PresentationPawnHidden
+            //PresentationPawnGhost
+            //PresentationPawnGhostAndOpaque
+
+        }
+    }
+}
---
+++ FPcameraSimple.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace HkFPCameraModSimple
+{
+	public class FPcameraSimple : MonoBehaviour
+	{
+		private float cameraSensitivity = 75;
+		private float climbSpeed = 4;
+		private float normalMoveSpeed = 10;
+
+		private float slowMoveFactor;
+		private float fastMoveFactor;
+
+		private float rotationX = 0.0f;
+		private float rotationY = 0.0f;
+
+		void Update()
+		{
+			rotationX += Input.GetAxis("Mouse X") * cameraSensitivity * Time.deltaTime;
+			rotationY += Input.GetAxis("Mouse Y") * cameraSensitivity * Time.deltaTime;
+			rotationY = Mathf.Clamp(rotationY, -90, 90);
+
+			transform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.up);
+			transform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
+
+			if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+				{ fastMoveFactor = 3; }
+            else 
+				{ fastMoveFactor = 1; }
+			if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
+				{ slowMoveFactor = 0.25f; }
+			else
+				{ slowMoveFactor = 1; }
+
+			transform.position += transform.forward * normalMoveSpeed * slowMoveFactor * fastMoveFactor * Input.GetAxis("Vertical") * Time.deltaTime;
+			transform.position += transform.right * normalMoveSpeed * slowMoveFactor * fastMoveFactor * Input.GetAxis("Horizontal") * Time.deltaTime;
+
+
+			if (Input.GetKey(KeyCode.Q)) { transform.position += transform.up * climbSpeed * slowMoveFactor * fastMoveFactor * Time.deltaTime; }
+			if (Input.GetKey(KeyCode.E)) { transform.position -= transform.up * climbSpeed * slowMoveFactor * fastMoveFactor * Time.deltaTime; }
+		}
+	}
+}
